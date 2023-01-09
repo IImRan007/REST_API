120350e10a782a1221855e3209f68e0cc629ff38
Mohd Imran <mohd78696321@gmail.com>
2023-01-09 15:28:26 +0530

    filteration & searching

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,12 @@ const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
   // Get all data from database
-  const data = await Product.find({});
+  const data = await Product.find(req.query);
   res.status(200).json({ data });
 };
 
 const getAllProductsTesting = async (req, res) => {
-  const data = await Product.find({});
+  const data = await Product.find(req.query);
   res.status(200).json({ data });
 };
 
