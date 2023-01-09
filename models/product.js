04fbf36e4c20e3beca3c8a90741df550f0547ad0
Mohd Imran <mohd78696321@gmail.com>
2023-01-09 14:04:59 +0530

    Added Product Schema

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const mongoogse = require("mongoose");
+
+const ProductSchema = new mongoogse.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: [true, "Price must be provided"],
+  },
+  feature: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.9,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["apple", "samsung", "dell", "mi"],
+      message: `{Value} is not supported`,
+    },
+  },
+});
+
+module.exports = mongoogse.model("Product", ProductSchema);
