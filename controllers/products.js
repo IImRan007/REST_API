d20b2d5759714dd682ff94b0d5c8ad948043b9b6
Mohd Imran <mohd78696321@gmail.com>
2023-01-09 16:02:00 +0530

    Added sort functionality

--- controllers/products.js
+++ controllers/products.js
@@ -8,6 +8,7 @@ const getAllProducts = async (req, res) => {
 
 const getAllProductsTesting = async (req, res) => {
   const data = await Product.find(req.query);
+  // const data = await Product.find(req.query).sort("name"); // sort name in ascending order use ("-name") for descending
   res.status(200).json({ data });
 };
 
